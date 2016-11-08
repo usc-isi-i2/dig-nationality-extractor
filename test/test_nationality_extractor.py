5e346dbe5765b83f7f88005c101936a046774061
jasonslepicka <jasonslepicka@gmail.com>
2016-11-07 20:37:14 -0800

    Update to 0.3.x extractor
    add ngrams for nationalities of two or more words
    lower case nationalities as preprocess step
    remove duplicated code

--- test/test_nationality_extractor.py
+++ test/test_nationality_extractor.py
@@ -1,14 +1,9 @@
-import sys
-import time
-import os
 import unittest
-
-# sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-# TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 from digExtractor.extractor import Extractor
 from digExtractor.extractor_processor import ExtractorProcessor
-from digNationalityExtractor.nationality_extractor import NationalityExtractor
+from digNationalityExtractor.names_helper import get_nationality_extractor
+
 
 class TestNationalityExtractorMethods(unittest.TestCase):
 
@@ -18,18 +13,46 @@ class TestNationalityExtractorMethods(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_business_type_extractor(self):
-        doc = {'content': ['American', 'Antiguans', 'Panamanian', 'Hello', 'World'], 'b': 'world'}
+    def test_nationality_type_extractor(self):
+        doc = {'content': ['American', 'Antiguans',
+                           'Panamanian', 'Hello', 'World'], 'b': 'world'}
 
-        extractor = NationalityExtractor().set_metadata({'extractor': 'nationality'})
-        extractor_processor = ExtractorProcessor().set_input_fields(['content']).set_output_field('extracted').set_extractor(extractor)
-        updated_doc = extractor_processor.extract(doc)
-        self.assertEqual(updated_doc['extracted'][0]['value'], ['Panamanian', 'American', 'Antiguans'])
+        extractor = get_nationality_extractor().set_metadata(
+            {'extractor': 'nationality'})
+        ep = ExtractorProcessor().set_input_fields(
+            'content').set_output_field('extracted').set_extractor(extractor)
+        updated_doc = ep.extract(doc)
+        result = updated_doc['extracted'][0]['result']
+        self.assertEqual(result[0]['value'], 'american')
+        self.assertEqual(result[1]['value'], 'antiguans')
+        self.assertEqual(result[2]['value'], 'panamanian')
 
-    
+    def test_nationality_type_extractor_context(self):
+        doc = {'content': ['American', 'Antiguans',
+                           'Panamanian', 'Hello', 'World', 'East', 'Timorese', 'kittian', 'and', 'nevisian', 'End'], 'b': 'world'}
+
+        extractor = get_nationality_extractor().set_metadata(
+            {'extractor': 'nationality'}).set_include_context(True)
+        ep = ExtractorProcessor().set_input_fields(
+            'content').set_output_field('extracted').set_extractor(extractor)
+        updated_doc = ep.extract(doc)
+        result = updated_doc['extracted'][0]['result']
+        self.assertEqual(result[0]['value'], 'american')
+        self.assertEqual(result[0]['context']['start'], 0)
+        self.assertEqual(result[0]['context']['end'], 1)
+        self.assertEqual(result[1]['value'], 'antiguans')
+        self.assertEqual(result[1]['context']['start'], 1)
+        self.assertEqual(result[1]['context']['end'], 2)
+        self.assertEqual(result[2]['value'], 'panamanian')
+        self.assertEqual(result[2]['context']['start'], 2)
+        self.assertEqual(result[2]['context']['end'], 3)
+        self.assertEqual(result[3]['value'], 'east timorese')
+        self.assertEqual(result[3]['context']['start'], 5)
+        self.assertEqual(result[3]['context']['end'], 7)
+        self.assertEqual(result[4]['value'], 'kittian and nevisian')
+        self.assertEqual(result[4]['context']['start'], 7)
+        self.assertEqual(result[4]['context']['end'], 10)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
